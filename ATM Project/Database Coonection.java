e999372b0483fb1f03dd427da7a603066aa06867
Priyanshu Negi <pnegi2703@gmail.com>
2025-09-10 17:10:02 +0530

    Update Database Coonection.java

--- ATM Project/Database Coonection.java
+++ ATM Project/Database Coonection.java
@@ -8,7 +8,7 @@ public class DatabaseConnection {
 	
 	private static final String USER ="root";
 	
-	private static final String PASSWORD ="Negi@123";
+	private static final String PASSWORD ="Enter Password Here";
 	
 	public static Connection getConnection() throws SQLException{
 		return DriverManager.getConnection(URL,USER,PASSWORD);
